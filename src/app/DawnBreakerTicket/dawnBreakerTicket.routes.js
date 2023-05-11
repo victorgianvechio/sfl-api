50b9aa4f3f2f13d4d92d9b8b70de685a9d0dd2e2
Victor Gianvechio <victor.gianvecchio@live.com>
2023-05-11 01:20:56 -0300

    retirado autenticação

--- src/app/DawnBreakerTicket/dawnBreakerTicket.routes.js
+++ src/app/DawnBreakerTicket/dawnBreakerTicket.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
 import DawnBreakerTicketController from './DawnBreakerTicketController';
-import authMiddleware from '../../middlewares/auth';
+// import authMiddleware from '../../middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/ranking', authMiddleware, DawnBreakerTicketController.ranking);
-// routes.get('/ranking', DawnBreakerTicketController.ranking);
+// routes.get('/ranking', authMiddleware, DawnBreakerTicketController.ranking);
+routes.get('/ranking', DawnBreakerTicketController.ranking);
 
 export default routes;
